5652195d12f8332f1ba6d75ce2660059c0e42140
MII <igoryha-701@mail.ru>
2023-03-28 22:56:02 +0500

    end2

--- db.py
+++ db.py
@@ -7,65 +7,22 @@ class Database:
         self.connection = sqlite3.connect(db_file, check_same_thread=False)
         self.cursor = self.connection.cursor()
 
-    def post_sigh(self, user_data):
-        l=[]
-        l1 = []
-        for key in user_data:
-            l.append(user_data[key])
-            l1.append(key)
-        l=tuple(l)
-        l1 = tuple(l1)
+    def create_database(self):
         with self.connection:
-            self.cursor.execute(f"INSERT INTO sigh {l1} VALUES {l}")
+            res = self.cursor.execute(f'''CREATE TABLE IF NOT EXISTS users
+            (id integer PRIMARY KEY AUTOINCREMENT,
+            chat_id BIGINT UNIQUE,
+            name text, 
+            email TEXT,
+            result INT)
+            ''')
+            return res
 
-    def count_sigh(self):
-        with  self.connection:
-            result=self.cursor.execute("SELECT COUNT(*) FROM sigh").fetchone()[0]
-            return result
-
-    def get_sigh(self,i):
-        with  self.connection:
-            # OFFSET = i - 1.txt
-            result=self.cursor.execute(f"SELECT * FROM sigh LIMIT 1 OFFSET {i}").fetchone()
-            logger.info(result)
-            return result
-
-
-    def get_all_sigh(self):
-        with self.connection:
-            result=[]
-            var = self.cursor.execute(f"SELECT * FROM sigh").fetchall()
-            logger.info(var)
-            stroka2 = "Список записей:\n"
-            i = 1
-            for results in var:
-                stroka2 = stroka2 + f"{i}. {results}\n"
-                i = i + 1
-            stroka2=stroka2+"\nНапишите номер записи, котурую хотите удалить?"
-            return stroka2
-
-
-    def delete_sigh(self, nomer):
-        delE = self.cursor.execute(f"DELETE FROM sigh WHERE id = ?", (nomer,))
-        logger.info(delE)
-
-    def delete_sigh2(self, rowid):
-        with self.connection:
-            self.cursor.execute(f"DELETE FROM sigh WHERE rowid = ?", (rowid,))
-
-    def status(self, nomer):
-        with self.connection:
-            result = self.cursor.execute(f"SELECT rowid FROM sigh").fetchall()
-            logger.info(result)
-            if len(result) >= nomer > 0:
-                index = result[nomer - 1]
-                logger.info(index)
-                rowid = index[0]
-                self.cursor.execute(f"UPDATE sigh SET status=0  WHERE rowid = ?", (rowid,))
-            else:
-                return False
-
-    def get_status(self):
-        with self.connection:
-            result = self.cursor.execute(f"SELECT rowid,id FROM sigh WHERE status = 0").fetchone()
-            return result
+    def post_result(self, chat_id, name, email, result):
+        l = (chat_id, name, email, result)
+        try:
+            with self.connection:
+                self.cursor.execute(f"INSERT INTO users (chat_id, name, email, result) VALUES {l}")
+        except:
+            with self.connection:
+                self.cursor.execute(f"UPDATE users SET name=?, email=?, result =? WHERE chat_id={chat_id}", (name, email, result))
